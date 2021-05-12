3d00a1488acb1b5be5f7fd4fca6cdf7a5730797b
Vasily Smolyar <smolyar4@gmail.com>
2021-05-12 18:31:44 +0300

    Выпадающий список

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import './App.css';
 import Candles from './components/Candles';
 import SearchBar from './components/SearchBar';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
 function App() {
-	const [ stock, setStock ] = useState({ticker: 'BAC'});
+	const [ stock, setStock ] = useState({
+		figi: "BBG000BCTLF6",
+		ticker: "BAC", 
+		isin: "US0605051046",
+		minPriceIncrement: 0.01,
+		lot: 1,
+		currency: "USD",
+		name: "Bank of America Corp",
+		type: "Stock"
+	});
 
 	return ( 
 		<div>
+			<CssBaseline />
 			<SearchBar setStock={setStock} />
 			<Candles ticker={stock.ticker}></Candles>
 		</div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Popper } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      border: '1px solid',
+      padding: theme.spacing(1),
+      backgroundColor: theme.palette.background.paper,
+    },
+}));
 
 export default function SearchBar({setStock}) {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popper' : undefined;
+
     const [ ticker, setTicker ] = useState('');
     const [ list, setList ] = useState([]);
 
@@ -10,10 +26,12 @@ export default function SearchBar({setStock}) {
 
         if(value === '') {
             setList([]);
+            setAnchorEl(null);
         } else {
             fetch(`http://localhost:5000/list/${value}`).then((res) => {
                 return res.json();
             }).then((res) => {
+                setAnchorEl(event.target);
                 setList(res);
             });
         }
@@ -29,10 +47,12 @@ export default function SearchBar({setStock}) {
 
     return (
         <div>
-            <input value={ticker} onChange={onChange} />
-            <ul>
-                {options}
-            </ul>
+            <TextField value={ticker} onChange={onChange} placeholder="Введите тикер бумаги" aria-describedby={id} />
+            <Popper anchorEl={anchorEl} id={id} open={open}>
+                <div className={classes.paper}>
+                    {options}
+                </div>
+            </Popper>
         </div>
     )
 }
